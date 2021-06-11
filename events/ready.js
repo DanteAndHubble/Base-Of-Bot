267d981f14fb31c2628b663d1137aaf5ea525c89
DanteAndHubble <73189037+DanteAndHubble@users.noreply.github.com>
2021-06-11 07:52:18 -0700

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,3 +1,6 @@
+const config = require('../settings.json')
+
 module.exports = bot => {
     console.log('Bot is online!')
+    client.user.setPresence({ activity: { name: `${config.Status}` }, status: 'ONLINE'}).catch(console.error);
 }
