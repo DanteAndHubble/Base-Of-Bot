8c5be6a68fb9cd06ae67b803b115108418fd5229
DanteAndHubble <73189037+DanteAndHubble@users.noreply.github.com>
2021-06-10 19:07:18 -0700

    Update message.js

--- events/message.js
+++ events/message.js
@@ -1,4 +1,4 @@
-const afk = require('../models/afk');
+const config = require('./settings.json');
 
 module.exports = async (bot, message) => {
   if (message.author.bot) return;
@@ -7,33 +7,7 @@ module.exports = async (bot, message) => {
   const cmd = messageArray[0];
   const args = messageArray.slice(1);
 
-  // ----------  AFK STUFF  ----------
-  const mentioned = message.mentions.members.first();
-  if (mentioned) {
-    const data = await afk.findOne({
-      UserID: mentioned.id,
-    });
-
-    if (data) {
-      message.channel.send(`**${mentioned.user.username}** is currently AFK. Reason: **${data.Reason}**`);
-    }
-  }
-
-  const mainData = await afk.findOne({
-    UserID: message.author.id,
-  });
-
-  if (mainData) {
-    message.channel.send(`**Welcome back!** I removed your AFK **${message.author.username}**.`);
-    mainData.deleteOne({
-      UserID: message.author.id,
-    });
-  }
-  // ----------  AFK STUFF  ----------
-
-  const prefix = '?';
-
-  if (message.content.match(new RegExp(`^<@!?${bot.user.id}>( |)$`))) return message.channel.send(`${message.guild.name}'s Prefix is \`${prefix}\`\n\nTo get a list of commands, say \`${prefix}help\``);
+  const prefix = config.defPrefix;
 
   if (!message.content.startsWith(prefix)) return;
   const commandfile = bot.commands.get(cmd.slice(prefix.length).toString().toLowerCase()) || bot.commands.get(bot.aliases.get(cmd.slice(prefix.length).toString().toLowerCase()));;
